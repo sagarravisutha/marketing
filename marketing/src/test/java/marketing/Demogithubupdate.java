e72b8e74d64a878216d247f3dfc7b57e76f1b185
User <User@DESKTOP-0RPOSRV>
2022-03-23 17:45:01 +0530

    new update

---
+++ marketing/src/test/java/marketing/Demogithubupdate.java
@@ -0,0 +1,11 @@
+package marketing;
+
+import org.testng.annotations.Test;
+
+public class Demogithubupdate {
+	@Test
+	public void githubupdate() {
+		System.out.println("new github update");
+	}
+
+}
